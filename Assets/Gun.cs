b1d318155d25d6f7912c4acd0670342a2c58e93e
Daniel <dmansfie.umd.edu>
2018-08-07 16:02:43 -0400

    added all existing work

---
+++ Assets/Gun.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 30f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impactEffect;
+
+	// Update is called once per frame
+	void Update () {
+	    // checks if player has fired
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            Drop();
+        }
+	}
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(muzzleFlash.transform.position, muzzleFlash.transform.forward, out hit, range))
+        {
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 1f);
+        }
+    }
+
+    void Drop()
+    {
+        GameObject.Find("sniperCamo").transform.SetParent(null);
+    }
+}
